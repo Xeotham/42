4a6e9b744eb69c28f209bd07ecb3777963aa63f5
mathaouas <mat.haouas@gmail.com>
2023-10-03 00:07:16 +0200

    test

---
+++ libft/ft_memset.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void    *ft_memset (void *str1, int value, unsigned int num)
+{
+    int i;
+    char    *str2;
+    
+    str2 = (char *) str1;
+    i = 0;
+    while (i < num - 1)
+    {
+        str2[i] = value;
+        i++;
+    }
+    return ((void *) str2);
+}
