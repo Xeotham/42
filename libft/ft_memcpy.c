c32bacd3cfd9edaeb9c0259bc0536a83d57682f8
mathaouas <mat.haouas@gmail.com>
2023-10-07 11:51:26 +0200

    ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -3,12 +3,27 @@
 void    *ft_memcpy(void *dest, const void *src, size_t num)
 {
     int i;
+    char *desttmp;
+    char *srctmp;
+
+    desttmp = (char *) dest;
+    srctmp = (char *) src;
 
     i = 0;
-    while (i <= num - 1)
+    while (i < num)
     {
-        dest[i] = src[i];
+        desttmp[i] = srctmp[i];
         i++;
     }
-    return(dest);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    char *src = "test1";
+    char *dest = "aaaaa";
+
+    ft_memcpy(&dest, &src, 4);
+    printf("%s", dest);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -1,5 +1,12 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
 # include <stdlib.h>
 # include <string.h>
 
 void    *ft_memset (void *str1, int value, size_t num);
 void    *ft_bzero(void *str, size_t num);
+void    *ft_memcpy(void *dest, const void *src, size_t num);
+char *  ft_strnew(size_t size);
+
+#endif
